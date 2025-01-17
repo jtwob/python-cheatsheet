f71d2a6340df96c6e13f0b0ec4cbad7e37715b67
James Totah <jtotah@usfca.edu>
2025-01-17 10:55:01 -0600

    OOP build out

--- oop.py
+++ oop.py
@@ -1,5 +1,14 @@
 class oop:
-    prop = 1
+    def __init__(self, prop1, prop2):
+        self.prop1 = prop1
+        self.prop2 = prop2
 
-p1 = oop()
-print(p1.prop)
+    def getProp1(self):
+        return self.prop1
+    def setProp1(self, newVal):
+        self.prop1 = newVal
+
+p1 = oop(4, 5)
+print(p1.getProp1())
+p1.setProp1(2)
+print(p1.getProp1())
