9dec233262a667ae318206b5d2a8687a6be6e184
James Totah <jtotah@usfca.edu>
2025-01-20 17:42:33 -0600

    added basic graph

--- matplotlib_sandbox.py
+++ matplotlib_sandbox.py
@@ -1,1 +1,8 @@
-import matplotlib
+import matplotlib, matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
