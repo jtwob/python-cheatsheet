e725f2c8713ab4b34d220350c4ad33aae70d5fe4
James Totah <jtotah@usfca.edu>
2025-01-21 23:49:44 -0600

    scatter plotting with matplotlib

--- matplotlib_sandbox.py
+++ matplotlib_sandbox.py
@@ -4,5 +4,13 @@ import numpy as np
 xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 
-plt.plot(xpoints, ypoints)
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+plt.scatter(x, y)
+
+# plt.plot(xpoints, ypoints)
+
+plt.grid()
+
 plt.show()
